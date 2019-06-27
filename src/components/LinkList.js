ee1b4d36c78966d9212d4b33949c3f82ebbe992b
ispoljari <ivanspoljaric22@gmail.com>
2019-06-27 17:15:06 +0200

    Implement data fetching with Query

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -17,22 +17,20 @@ const FEED_QUERY = qgl`
 
 export class LinkList extends Component {
   render() {
-    const linksToRender = [
-      {
-        id: '1',
-        description: 'Prisma turns your database into a GraphQL API 😎',
-        url: 'https://www.prismagraphql.com',
-      },
-      {
-        id: '2',
-        description: 'The best GraphQL client',
-        url: 'https://www.apollographql.com/docs/react/',
-      },
-    ]
-
     return (
       <Query query={FEED_QUERY}>
-        {() => linksToRender.map(link => <Link key={link.id} link={link} />)}
+        {({ loading, error, data }) => {
+          if (loading) return <div>Fetching</div>
+          if (error) return <div>Error</div>
+
+          const linksToRender = data.feed.links;
+
+          return (
+            <div>
+              {linksToRender.map(link => <Link key={link.id} link={link} />)}
+            </div>
+          )
+        }}
       </Query>
     )
   }
